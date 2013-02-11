6b589cdab35b01b15d3e933e8519211088798f37
Hassan Shahid <set5think@gmail.com>
2013-02-10 21:41:50 -0500

    adding gitignore and modified postrello.rb before
    adding rails engine

--- postrello.rb
+++ postrello.rb
@@ -1,9 +1,9 @@
 require 'trello'
 
-TRELLO_DEVELOPER_PUBLIC_KEY = 'XXX'
-TRELLO_MEMBER_TOKEN = 'XXX'
+TRELLO_DEVELOPER_KEY = 'xxx'
+TRELLO_SECRET = 'xxx'
 
 Trello.configure do |config|
-    config.developer_public_key = TRELLO_DEVELOPER_PUBLIC_KEY
-    config.member_token = TRELLO_MEMBER_TOKEN
+    config.developer_public_key = TRELLO_DEVELOPER_KEY
+    config.member_token = TRELLO_SECRET
 end
